92f2adcad55dfbfcbcd1822e2beee2b0b80ea2a9
Willian <willianmarcos907@gmail.com>
2022-10-29 16:23:53 -0300

    new controller

--- src/services/connect.js
+++ src/services/connect.js
@@ -17,17 +17,3 @@ const query = (text, param) => {
 module.exports = {
     query: query
 };
-
-
-//CONFIGURAÇÕES HEROKU
-
-// Host
-// ec2-44-194-92-192.compute-1.amazonaws.com
-// Database
-// dbrldm1n01keid
-// User
-// tzkujbwjrknjoi
-// Port
-// 5432
-// Password
-// d9a3c581aa567ab3859cbaba21aae226a0f0a24c599fb2c5705ac19cd0fc0d85
